7ea9fca2a83af0556e3320b83f20e04b21316678
Ciro23 <ciro.sorre23@gmail.com>
2022-12-11 19:22:23 +0100

    refactoring day1

--- src/main/java/it/tino/adventofcode/day1/CaloriesCarried.java
+++ src/main/java/it/tino/adventofcode/day1/CaloriesCarried.java
@@ -1,25 +1,30 @@
 package it.tino.adventofcode.day1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import it.tino.adventofcode.FileLinesReader;
+
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 public class CaloriesCarried {
 
     public static void main(String[] args) {
         CaloriesCarried caloriesCarried = new CaloriesCarried();
-        List<String> lines = caloriesCarried.getFileLines("calories-carried-by-elves.txt");
+        FileLinesReader fileLinesReader = new FileLinesReader();
+
+        List<String> lines = fileLinesReader.getFileLines("calories-carried-by-elves.txt");
         List<Integer> caloriesCarriedByElves = caloriesCarried.getCaloriesCarriedByElves(lines);
 
         Integer maximumCaloriesCarried = Collections.max(caloriesCarriedByElves);
         System.out.println("The elf carrying the most calories got " + maximumCaloriesCarried);
     }
 
+    /**
+     * Each elf writes the calories for all the food
+     * their carrying in a document, and separates
+     * their own inventory from the previous Elf's inventory
+     * (if any) by a blank line
+     */
     public List<Integer> getCaloriesCarriedByElves(List<String> lines) {
         int currentElf = 0;
 
@@ -41,27 +46,5 @@ public class CaloriesCarried {
         }
 
         return caloriesCarriedByElves;
-    }
-
-    public List<String> getFileLines(String filename) {
-        InputStream inputStream = this.getClass()
-                .getClassLoader()
-                .getResourceAsStream(filename);
-
-        if (inputStream == null) {
-            return Collections.emptyList();
-        }
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        LinkedList<String> lines = new LinkedList<>();
-        try {
-            while (bufferedReader.ready()) {
-                lines.addLast(bufferedReader.readLine());
-            }
-        } catch (IOException e) {
-            return Collections.emptyList();
-        }
-
-        return lines;
     }
 }
